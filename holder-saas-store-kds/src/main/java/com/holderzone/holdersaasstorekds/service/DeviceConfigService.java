b6bf6c7ca30179f47ddd114a098fc56ff5f9bdff
彭启 <762687633@qq.com>
2020-01-09 23:56:43 +0800

    [pengqi] nothing

--- holder-saas-store-kds/src/main/java/com/holderzone/holdersaasstorekds/service/DeviceConfigService.java
+++ holder-saas-store-kds/src/main/java/com/holderzone/holdersaasstorekds/service/DeviceConfigService.java
@@ -2,9 +2,17 @@ package com.holderzone.holdersaasstorekds.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.holderzone.holdersaasstoredto.dto.kds.req.DeviceCreateReqDTO;
+import com.holderzone.holdersaasstoredto.dto.kds.req.DeviceQueryReqDTO;
 import com.holderzone.holdersaasstoredto.dto.kds.resp.DeviceStatusRespDTO;
 import com.holderzone.holdersaasstorekds.entity.domain.DeviceConfigDO;
 
 public interface DeviceConfigService extends IService<DeviceConfigDO> {
+
     DeviceStatusRespDTO create(DeviceCreateReqDTO deviceCreateReqDTO);
+
+    DeviceStatusRespDTO query(DeviceQueryReqDTO deviceQueryReqDTO);
+
+    void initialize(DeviceQueryReqDTO deviceQueryReqDTO);
+
+    DeviceConfigDO queryDeviceByGuid(String storeGuid, String deviceId);
 }
