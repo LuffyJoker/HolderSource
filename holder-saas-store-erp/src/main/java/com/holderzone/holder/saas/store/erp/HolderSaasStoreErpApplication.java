b32e6d44722211786340d8826abbbeb15eb913be
彭启 <762687633@qq.com>
2020-01-11 20:00:19 +0800

    [pengqi] nothing

---
+++ holder-saas-store-erp/src/main/java/com/holderzone/holder/saas/store/erp/HolderSaasStoreErpApplication.java
@@ -0,0 +1,13 @@
+package com.holderzone.holder.saas.store.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolderSaasStoreErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolderSaasStoreErpApplication.class, args);
+    }
+
+}
---
+++ holder-saas-store-erp/src/test/java/com/holderzone/holder/saas/store/erp/HolderSaasStoreErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holderzone.holder.saas.store.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolderSaasStoreErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
